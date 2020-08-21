dea51f79fb64511436bcb9a4930c4d4de3a03eea
dkarmy12 <55491427+dkarmy12@users.noreply.github.com>
2020-08-21 23:09:20 +0530

    Add files via upload

---
+++ migrations/versions/693de4410a10_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 693de4410a10
+Revises: 
+Create Date: 2020-08-21 23:06:47.558595
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '693de4410a10'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
